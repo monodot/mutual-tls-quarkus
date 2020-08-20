963bd03d1504ec778b4be063b3e05c712922877e
quarkusio <no-reply@quarkus.io>
2020-08-20 15:38:04 +0000

    Initial commit

---
+++ src/test/java/xyz/tomd/demos/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package xyz.tomd.demos;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
